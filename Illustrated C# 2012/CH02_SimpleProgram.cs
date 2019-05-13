41f559a572530a76adb0d0f39d1174888535be84
XinRuoShi <XinruoShi>
2019-05-13 15:32:09 +0800

    IllustratedC#2012 CH02 CH03

---
+++ Illustrated C# 2012/CH02_SimpleProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IllustratedCSharp2012
+{
+    public class CH02_SimpleProgram
+    {
+        public static void Base()
+        {
+            Console.WriteLine("Hello World!");
+
+            int var1 = 5;
+            Console.WriteLine("The value of var1 is {0}",var1);
+
+            Console.WriteLine("Three integers are {1},{0},{1}", 3, 6);
+
+            //Formatting Numeric Strings
+            Console.WriteLine("The value:|{0,10:C}|",500);
+            Console.WriteLine("The value:|{0,-10:C}|", 500);
+
+            /*  */
+            //
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Illustrated C# 2012/CH03_SimpleProgram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IllustratedCSharp2012
+{
+    class CH03_SimpleProgram
+    {
+        static void Base()
+        {
+            // Predefined Types
+            sbyte b1 = -10;
+            byte b2 = 10;
+
+            short s1 = -10;
+            ushort s2 = 10;
+
+            int i1 = -10;
+            uint i2 = 10;
+
+            long l1 = -10;
+            ulong l2 = 10;
+
+            float f = 3.14F;
+            double d = 3.14; // default
+            decimal d1 = 3.14M;
+
+            bool b = true;
+            char c = 'A', c2 = '\u0000';
+
+            object o = new object();
+            string s = "String";
+            /* dynamic d =; */
+        }
+        //User-Defined Types
+        class A { }
+        interface I { }
+        struct B { }
+        enum C { }
+        delegate void D();
+        Array E;
+
+
+
+    }
+}
